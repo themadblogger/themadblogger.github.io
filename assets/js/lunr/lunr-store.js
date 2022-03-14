8764c46006d6592fc71aae57ce9e96bc97bf66e4
ChrisHammond <ChrisHammond@users.noreply.github.com>
2022-03-14 19:45:55 +0000

    deploy: 01ada48aa35023900796cc5e9871211338b542fc

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -348,7 +348,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Drinking and driving, lets sue!",
-        "excerpt":"http://www.stltoday.com/stltoday/news/stories.nsf/stlouiscitycounty/story/B30B1CD42616DCDC862572E5005F04FC?OpenDocument  Shame on the Hancock family if they actually do go through with this rediculous law suit. Their son was drinking and driving, and killed himself. It's his fault, not anyone else's. The Mad Blogger is sorry for your loss but this isn't anyone's fault but Josh Hancock's. ","categories": [],
+        "excerpt":"2022 Update: This blog post was not a highlight for TMB. Apologies if the Hancock family took offense. Sorry for your loss. http://www.stltoday.com/stltoday/news/stories.nsf/stlouiscitycounty/story/B30B1CD42616DCDC862572E5005F04FC?OpenDocument (link no longer valid) Shame on the Hancock family if they actually do go through with this rediculous law suit. Their son was drinking and driving, and...","categories": [],
         "tags": [],
         "url": "https://www.themadblogger.com/Drinking-and-driving-lets-sue",
         "teaser": null
--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -318,7 +318,7 @@ var documents = [{
     "id": 63,
     "url": "https://www.themadblogger.com/Drinking-and-driving-lets-sue",
     "title": "Drinking and driving, lets sue!",
-    "body": "2007/05/24 - http://www. stltoday. com/stltoday/news/stories. nsf/stlouiscitycounty/story/B30B1CD42616DCDC862572E5005F04FC?OpenDocument Shame on the Hancock family if they actually do go through with this rediculous law suit. Their son was drinking and driving, and killed himself. It's his fault, not anyone else's. The Mad Blogger is sorry for your loss but this isn't anyone's fault but Josh Hancock's. "
+    "body": "2007/05/24 - 2022 Update: This blog post was not a highlight for TMB. Apologies if the Hancock family took offense. Sorry for your loss. http://www. stltoday. com/stltoday/news/stories. nsf/stlouiscitycounty/story/B30B1CD42616DCDC862572E5005F04FC?OpenDocument (link no longer valid) Shame on the Hancock family if they actually do go through with this rediculous law suit. Their son was drinking and driving, and killed himself. It’s his fault, not anyone else’s. The Mad Blogger is sorry for your loss but this isn’t anyone’s fault but Josh Hancock’s. "
     }, {
     "id": 64,
     "url": "https://www.themadblogger.com/Who-is-better-Directv-or-Dish",
